42e0a9fbdc902dfb431cb1e58a739c923c25c5ed
Milena Fleming <milenasapucaia29@gmail.com>
2023-05-30 20:30:00 +0100

    feat(database): created databases, factories, seeders and relation

---
+++ database/factories/WorkerFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Worker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Worker>
+ */
+class WorkerFactory extends Factory
+{
+    protected $model = Worker::class;
+
+    public function definition(): array
+    {
+        return [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'job_title' => $this->faker->jobTitle(),
+            'email' => $this->faker->unique()->safeEmail(),
+        ];
+    }
+}
